77a6e03964412d1f1b1946de6d9894f735785d67
EnigmaDynamicTechServices <enigma.dts@gmail.com>
2024-09-01 09:29:23 +0545

    validation added

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,41 @@
+import { useState } from 'react'
 import '../styles/login.css'
-const Login = ()=>{
-    return(
-        <div id="container">
-            <div className='field'>
-                <label for="email">Email</label><br/>
-                <input type='email' placeholder="Enter your email" name='email'/>
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("submitted")
+        if(password.length < 6){
+            alert("Password must be at least 6 characters long")
+            return
+        }
+        console.log(email, password)
+    }
+    const handleOnEmailChange = (e) => {
+        console.log("emal : ", e.target.value)
+        setEmail(e.target.value)
+    }
+    const handleOnPasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div id="container">
+                <div className='field'>
+                    <label for="email">Email</label><br />
+                    <input type='email' placeholder="Enter your email" name='email' onChange={handleOnEmailChange} value={email} required />
+                </div>
+                <div className='field'>
+                    <label for="password">Password</label><br />
+                    <input type='password' placeholder='Enter your password' name='password' onChange={handleOnPasswordChange} value={password} required />
+                </div>
+                <div>
+                    <input id='login-button' type='submit' value="Login"/>
+                </div>
             </div>
-            <div className='field'>
-                <label for="password">Password</label><br/>
-                <input type='password' placeholder='Enter your password' name='password'/>
-            </div>
-            <div>
-                <input id='login-button' type='submit' value="Login"/>
-            </div>
-        </div>
+        </form>
     );
 }
 export default Login
