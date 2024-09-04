21440a072808df752d47e7e526eafccb879cc8e0
EnigmaDynamicTechServices <enigma.dts@gmail.com>
2024-09-04 08:01:03 +0545

    register form state defined

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,42 +1,48 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import CustomFormField from "../components/custom_form_field";
 import "../styles/register.css"
 
 const Register = () => {
-    const fullNameRef = useRef("")
-    const handleRegister = () => {
-        var fullname = fullNameRef.current
-        console.log("fullname = ", fullname)
+    const [fullName, setFullName] = useState("")
+    const [username, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [gender, setGender] = useState("male")
+    const handleRegister = (event) => {
+        event.preventDefault()
+        console.log("fullname = ", fullName, username, email, phoneNumber, password, confirmPassword, gender)
     }
     return (
         <form onSubmit={handleRegister}>
             <div id="register-box">
                 <h1 id="title"><span id="re">Re</span>gistration</h1>
                 <div className="row-field">
-                    <CustomFormField label="Full Name" placeholder="Enter your full name" type="text" value="" cName="fullname" ref={fullNameRef} />
-                    <CustomFormField label="Username" placeholder="Enter your username" type="text" value="" cName="username" />
+                    <CustomFormField label="Full Name" placeholder="Enter your full name" type="text" cName="fullname"  onChange={(e) => setFullName(e.target.value)} value={fullName}/>
+                    <CustomFormField label="Username" placeholder="Enter your username" type="text" cName="username" onChange={(e) => setUserName(e.target.value)} value={username}/>
                 </div>
                 <div className="row-field">
-                    <CustomFormField label="Email" placeholder="Enter your email" type="text" value="" cName="email" />
-                    <CustomFormField label="Phone Number" placeholder="Enter strong Phone Number" type="text" value="" cName="phoneNumber" />
+                    <CustomFormField label="Email" placeholder="Enter your email" type="text" cName="email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <CustomFormField label="Phone Number" placeholder="Enter strong Phone Number" type="text" value={phoneNumber} cName="phoneNumber" onChange={(e) => setPhoneNumber(e.target.value)}/>
                 </div>
                 <div className="row-field">
-                    <CustomFormField label="Password" placeholder="Enter strong Password" type="text" value="" cName="password" />
-                    <CustomFormField label="Confirm Password" placeholder="Enter your Confirm Password" type="text" value="" cName="confirmPassword" />
+                    <CustomFormField label="Password" placeholder="Enter strong Password" type="text" value={password} cName="password" onChange={(e) => setPassword(e.target.value)}/>
+                    <CustomFormField label="Confirm Password" placeholder="Enter your Confirm Password" type="text" value={confirmPassword} cName="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)}/>
                 </div>
                 <div className="gender-field">
                     <p>Gender</p>
                     <div id="gender">
                         <div className="g">
-                            <input type="radio" name="gender" value="male" />
+                            <input type="radio" name="gender" value="male" onChange={(e) => setGender(e.target.value)}/>
                             <label for="male">Male</label>
                         </div>
                         <div className="g">
-                            <input type="radio" name="gender" value="female" />
+                            <input type="radio" name="gender" value="female" onChange={(e)=>setGender(e.target.value)}/>
                             <label for="female">Female</label>
                         </div>
                         <div className="g">
-                            <input type="radio" name="gender" value="other" />
+                            <input type="radio" name="gender" value="other" onChange={(e)=>setGender(e.target.value)}/>
                             <label for="male">Other</label>
                         </div>
                     </div>
